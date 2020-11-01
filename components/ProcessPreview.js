bae5fa0dad8f4a5e97d96b89c878ab8f1006259a
barz101 <barz100@gmail.com>
2020-11-02 01:41:52 +0200

    first commit

---
+++ components/ProcessPreview.js
@@ -0,0 +1,8 @@
+export default function ProcessPreview (props) {
+    console.log(props);
+return (
+    <div>
+        single ProcessPreview
+    </div>
+)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,60 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+// import styles from '../styles/Home.module.css'
+import useSWR from 'swr'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { request, gql } from 'graphql-request'
+// import fetcher from '../lib/fetcher.js'
+// import { request } from 'graphql-request'
+const fetcher = query => request(API, query)
+const QUERY = gql`
+  {
+    viewer {
+ site {
+  name
+  orgName
+  processes {
+    items {  
+      processId
+      description
+      name
+    }
+ }
 }
+}
+  }
+`
+const API = 'https://omni-prerelease.insights.us/api/graphql'
+
+function Home() {
+  // console.log(data);
+  const { data, error } = useSWR(
+    QUERY,
+    fetcher
+  )
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>blablalalalal</p>
+        {data &&
+        <p>{data.viewer.site.name}</p> }
+        {/* {data && data.viewer.processes.items.map((process) =>
+          <ProcessPreview process={process} key={processId} />
+        )} */}
+      </section>
+        
+    </Layout>
+  )
+
+}
+// export async function getServerSideProps() {
+//   // Fetch data from external API
+//   const data = request(API, query).then((res) => console.log(res))
+//   console.log(data);
+//   // Pass data to the page via props
+//   return { props: { data } }
+// }
+export default Home
